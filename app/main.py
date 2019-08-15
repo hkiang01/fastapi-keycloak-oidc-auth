554fc24afc878a091084516669b9e5f3a344c31b
Harrison Kiang <hkiang01@gmail.com>
2019-08-14 22:30:42 -0500

    url to get code found

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
+import logging
+
 import uvicorn
 from fastapi import FastAPI
+
+LOGIN_URL = "http://localhost:8080/auth/realms/Clients/protocol/openid-connect/auth?client_id=app&response_type=code"
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel("DEBUG")
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root(code: str):
+    logger.debug(f"code: {code}")
     return {"message": "Hello World"}
 
 
